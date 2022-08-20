cb36d9bca7b7216fa0b66a1fc753caaa91751860
Roel Moeyersoons <Roel.Moeyersoons@net-it.be>
2022-08-20 15:40:00 +0200

    Add some tests

---
+++ Backend/Backend.ApplicationTests/Extensions/PersonExtensionsTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Backend.Application.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Backend.Model;
+
+namespace Backend.Application.Extensions.Tests
+{
+    public class PersonExtensionsTests
+    {
+
+        [TestMethod()]
+        [DataRow("John", "Doe", "eoD nhoJ")]
+        [DataRow("Roel", "", "leoR")]
+        public void ReverseName_reverses_name(string firstName, string lastName, string validResult)
+        {
+            var person = new Person
+            {
+                FirstName = firstName,
+                LastName = lastName
+            };
+
+            var result = person.ReverseName();
+
+            Assert.AreEqual(validResult, result);
+        }
+
+        [TestMethod()]
+        public void ReverseName_should_reverse_if_only_lastname()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ReverseName_should_reverse_if_only_firstname()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ Backend/Backend.ApplicationTests/Operations/AddPersonTests.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Backend.Application.Operations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using Backend.Application.Repository;
+using Backend.Model;
+using Microsoft.EntityFrameworkCore;
+using Backend.Application.Factories;
+using Bogus;
+
+namespace Backend.Application.Operations.Tests
+{
+    [TestClass()]
+    public class AddPersonTests
+    {
+
+        public class myTestDbFactory : IPersonRepositoryFactory
+        {
+            public Mock<IPersonRepository> Mock { get; set; }
+
+            public IPersonRepository GetRepository()
+            {
+                return Mock.Object;
+            }
+        }
+
+        [TestMethod()]
+        public async Task ExecuteAsyncTest()
+        {
+            var dbContextMock = new Mock<IPersonRepository>();
+            var dbSetMock = new Mock<DbSet<Model.Person>>();
+
+            var mockedFactory = new myTestDbFactory
+            {
+                Mock = dbContextMock
+            };
+
+            var faker = new Faker();
+
+            // further initialize mock for test case
+
+            var addPersonOperation = new AddPerson(mockedFactory);
+
+            var addpersonParams = new AddPersonParams
+            {
+                FirstName = faker.Random.Word(),
+                LastName = faker.Random.Word(),
+            };
+
+            await addPersonOperation.ExecuteAsync(addpersonParams);
+            
+            //do your asserts here, compare input params with what's passed to the mock
+
+            Assert.IsTrue(true);
+        }
+    }
+}
